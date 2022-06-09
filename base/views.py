bd62d4ce417632f9812912e5e29b52cf0762324f
vipin-2022 <vipincpy123@gmail.com>
2022-06-09 16:16:39 +0530

    Completed CRUD

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 from .models import Task
 
 class TaskList(ListView):
@@ -11,4 +13,18 @@ class TaskDetail(DetailView):
     model = Task 
     context_object_name='task'
 
-        
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url=reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url=reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task 
+    context_object_name='task'
+    success_url=reverse_lazy('tasks')
